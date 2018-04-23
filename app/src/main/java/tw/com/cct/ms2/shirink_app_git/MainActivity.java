a0569f876512084c5d7c31b9fcd2b644f7481bd2
kaochuyang <gordon209.tem97@g2.nctu.edu.tw>
2018-04-23 21:52:36 +0800

    處理大部分的單頁設定內容，利用dialog彈出視視窗去處理，坐到行人觸動開關的時候卡住，不知道為什麼edix.getText.tostring沒辦法正常讀取，還差約五個頁面要處理。

--- app/src/main/java/tw/com/cct/ms2/shirink_app_git/MainActivity.java
+++ app/src/main/java/tw/com/cct/ms2/shirink_app_git/MainActivity.java
@@ -10,7 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Base_activity {
 
@@ -20,7 +24,7 @@ public class MainActivity extends Base_activity {
     }
 
     ;
-    private final CharSequence[] item = {"", "item2", "item3"};
+    private final CharSequence[] items = {"允許現場設定", "不允許現場設定", "只允許現場查看"};
 
 
     @Override
@@ -33,9 +37,12 @@ public class MainActivity extends Base_activity {
         Button_goto_where(goto_tod_setting, intent_tod_setting_xml);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
-        final Button password_setting = findViewById(R.id.password_setting);
-
+        Button password_setting = findViewById(R.id.password_setting);
+        final Button Uart_select = findViewById(R.id.Uart_select);
+        Button datatbase_setting = findViewById(R.id.database_setting);
         FloatingActionButton setting_button_group = (FloatingActionButton) findViewById(R.id.setting_button_group);
+        Button report_cycle_setting = findViewById(R.id.report_cycle_setting);
+        Button ped_button_setting = findViewById(R.id.ped_button_setting);
         Log.d("test2", "onCreate: ");
         floating_button_function(setting_button_group, MainActivity.this);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +61,31 @@ public class MainActivity extends Base_activity {
                 password_edit();
             }
 
+        });
+        ped_button_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ped_button_edit();
+            }
+        });
+        datatbase_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database_setting();
+            }
+        });
+        report_cycle_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setReport_cycle();
+            }
+        });
+
+        Uart_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                com2_select();
+            }
         });
 
 
@@ -94,5 +126,111 @@ public class MainActivity extends Base_activity {
                 .show();
 
     }
+
+    private void ped_button_edit() {
+        final View ped_button_setting = LayoutInflater.from(MainActivity.this).inflate(R.layout.ped_button_setting, null);
+        new AlertDialog.Builder(MainActivity.this).setTitle(R.string.ped_button_edit)
+                .setView(ped_button_setting)
+
+                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Switch ped_switch = (Switch) findViewById(R.id.ped_button_switch);
+                                EditText plan_num_edit = (EditText) findViewById(R.id.plan_num_edit);
+                                String plan_ped =plan_num_edit.getText().toString();
+                                Log.d(plan_ped, "Editex: ");
+                            //    Toast.makeText(getApplicationContext(),  plan_ped, Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                )
+                .show();
+
+    }
+
+    int database_manual_manual_ChoiceIndex = 1;//把database_manual_ChiceIndex改成讀出來的內容
+
+    private void database_setting()//資料庫操作設定
+    {
+        final List<String> database_manual = new ArrayList<>();
+        database_manual.add(getString(R.string.allow_construction_setting));
+        database_manual.add(getString(R.string.refuse_construction_setting));
+        database_manual.add(getString(R.string.allow_construction_view));
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setSingleChoiceItems(database_manual.toArray(new String[database_manual.size()]), database_manual_manual_ChoiceIndex,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                database_manual_manual_ChoiceIndex = which;
+                                Toast.makeText(MainActivity.this, "你選擇的是" + database_manual.get(database_manual_manual_ChoiceIndex), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    int com2_select_ChoiceIndex = 0;
+
+    private void com2_select() {
+        final List<String> com2_select = new ArrayList<>();
+        com2_select.add(getString(R.string.GPS_link));
+        com2_select.add(getString(R.string.Red_countdown));
+        new AlertDialog.Builder(MainActivity.this)
+                .setSingleChoiceItems(com2_select.toArray(new String[com2_select.size()]), com2_select_ChoiceIndex,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                com2_select_ChoiceIndex = which;
+                                Toast.makeText(MainActivity.this, "你選擇的是" + com2_select.get(com2_select_ChoiceIndex), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+
+    }
+
+    int report_cycle_ChoiceIndex = 5;
+
+    private void setReport_cycle() {
+        final List<String> Report_cycle = new ArrayList<>();
+        Report_cycle.add(getString(R.string.stop_report_hardcore_state));
+        Report_cycle.add(getString(R.string.second_1));
+        Report_cycle.add(getString(R.string.second_2));
+        Report_cycle.add(getString(R.string.second_5));
+        Report_cycle.add(getString(R.string.minute_1));
+        Report_cycle.add(getString(R.string.minute_5));
+        new AlertDialog.Builder(MainActivity.this)
+                .setSingleChoiceItems(Report_cycle.toArray(new String[Report_cycle.size()]), report_cycle_ChoiceIndex,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                report_cycle_ChoiceIndex = which;
+                                Toast.makeText(MainActivity.this, "你選擇的是" + Report_cycle.get(report_cycle_ChoiceIndex), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+
+    }
+
 
 }
